09a242397b036d8c2b55a5a4935d87327ca30722
ityupanov <igor.tyupanov@gmail.com>
2022-03-29 23:02:28 +0300

    Howe Work 2.

---
+++ src/main/java/tracker/Main.java
@@ -0,0 +1,32 @@
+package tracker;
+
+import java.util.Scanner;
+
+public class Main {
+    private static final int WEEK = 5;
+    public static void main(String[] args) {
+        Scanner bugReport = new Scanner(System.in);
+        String name;
+        String priority;
+        int days = 0;
+        boolean daysOfWeek = false;
+
+        System.out.println("Enter name:");
+        name = bugReport.nextLine();
+        System.out.println("Enter priority: Trivial Minor Major Critical Blocker");
+        priority = bugReport.nextLine();
+
+        if ((priority.equals("Trivial")) || (priority.equals("Minor"))
+                || (priority.equals("Major")) || (priority.equals("Critical")) || (priority.equals("Blocker"))) {
+            System.out.println("Enter days: ");
+            days = bugReport.nextInt();
+            if (days > WEEK) {
+                daysOfWeek = true;
+            }
+        } else {
+            System.err.println("Error type");
+        }
+
+        System.out.println("Name: "+name+" Priority: "+priority+" Days: "+days+" More work week?: "+daysOfWeek);
+    }
+}
