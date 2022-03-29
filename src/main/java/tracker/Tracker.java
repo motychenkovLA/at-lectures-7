0b1b4928d7fc291a68b6161f2bfb1b4db2ebe75c
Albert G. Khusnetdinov <akhusnetdinov@t1-consulting.ru>
2022-03-29 17:11:51 +0300

    ДЗ_2

---
+++ src/main/java/tracker/Tracker.java
@@ -0,0 +1,23 @@
+package tracker;
+
+import javax.xml.transform.sax.SAXSource;
+import java.util.Scanner;
+
+public class Tracker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите резюме дефекта");
+        String summary = scanner.nextLine();
+
+        System.out.println("Введите критичность дефекта из списка\n"+"trivial, minor, major, critical, blocker");
+        String critical = scanner.nextLine();
+
+        System.out.println("Введите количество дней на исправление");
+        int countDay = scanner.nextInt();
+
+        System.out.println(summary+", "+critical+", "+countDay+", Исправление займет больше рабочей недели: "+(countDay > 5));
+
+        scanner.close();
+    }
+}
