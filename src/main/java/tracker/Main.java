3ce8e352316833db182ce1eb4f4cbd38ce743612
Julia Maple <klyonova1997@gmail.com>
2022-03-30 20:40:53 +0300

    finished homework2

--- src/main/java/tracker/Main.java
+++ src/main/java/tracker/Main.java
@@ -1,7 +1,16 @@
 package tracker;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Please, enter a description of the problem");
+        Scanner scanner = new Scanner(System.in);
+        String description = scanner.nextLine();
+        System.out.println("Please, enter a severity of the problem:\nS1 - Blocker;\nS2 - Critical;\nS3 - Major;\nS4 - Minor;\nS5 - Trivial");
+        String severity = scanner.nextLine();
+        System.out.println("How many days do you need to fix the problem?");
+        int numberOfDays =scanner.nextInt();
+        System.out.println("Description: " + description + "\nSeverity: " + severity + "\nWorkload in days: " + numberOfDays);
     }
 }
