02a77be24c4e7701b23050f847b537169f1a658a
Julia Maple <klyonova1997@gmail.com>
2022-03-30 21:18:46 +0300

    fix 1 homework2

--- src/main/java/tracker/Main.java
+++ src/main/java/tracker/Main.java
@@ -7,10 +7,18 @@ public class Main {
         System.out.println("Please, enter a description of the problem");
         Scanner scanner = new Scanner(System.in);
         String description = scanner.nextLine();
-        System.out.println("Please, enter a severity of the problem:\nS1 - Blocker;\nS2 - Critical;\nS3 - Major;\nS4 - Minor;\nS5 - Trivial");
+        System.out.println("Please, enter a severity of the problem:\nS1 - Blocker;\nS2 - Critical;" +
+                "\nS3 - Major;\nS4 - Minor;\nS5 - Trivial");
         String severity = scanner.nextLine();
         System.out.println("How many days do you need to fix the problem?");
         int numberOfDays =scanner.nextInt();
-        System.out.println("Description: " + description + "\nSeverity: " + severity + "\nWorkload in days: " + numberOfDays);
+
+        //check: 1 week include number of days for fix or not
+        int week = 7;
+        boolean result = numberOfDays > week;
+
+        System.out.println("Description: " + description + "\nSeverity: " +
+                severity + "\nWorkload in days: " + numberOfDays +
+                "\nNumber of days for fix more then one week: " + result);
     }
 }
