b781eb88ad805bf317d7609ef54f14b99d048dd8
lmotychenkov <motychenkovLA@gmail.com>
2022-04-04 18:03:07 +0300

    дз 3 замечания

--- src/main/java/tracker/Main.java
+++ src/main/java/tracker/Main.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        // todo 1+ - ни о чем не говорящее название x3
+        // todo 3 - 10 константой
         String[] arrayDefect = new String[10];
         String[] arrayDefect1 = new String[10];
         int[] arrayDefect2 = new int[10];
@@ -21,11 +23,12 @@ public class Main {
                     }
                     System.out.println("//Создание баг-репорта//");
                     System.out.println("Введите название дефекта");
+                    // todo 1 - можно сразу заносить в массивы, не перекладывая в переменные
                     String name = scanner.nextLine();
                     System.out.println("Введите критичность дефекта:\n" + "(trivial, minor, major, critical, blocker)");
                     String critical = scanner.nextLine();
                     System.out.println("Введите ожидаемое кол-во дней на исправление дефекта");
-                    int countDay = scanner.nextInt();
+                    int countDay = scanner.nextInt(); // todo 3+ - перенос курсора на чтение
                     System.out.println();
                     arrayDefect[i] = name;
                     arrayDefect1[i] = critical;
@@ -33,6 +36,7 @@ public class Main {
                     i++;
                     break;
                 case "list":
+                    // todo 5 - выход за пределы введенных дефектов, выводятся null-ы
                     for (int a = 0; a < arrayDefect.length; a++) {
                         System.out.println("Номер дефекта: " + a + ", Название: " + arrayDefect[a] + ", Критичность: "
                                 + arrayDefect1[a] + ", Кол-во дней: " + arrayDefect2[a]);
@@ -45,6 +49,7 @@ public class Main {
                 default:
                     System.out.println("try again");
                     System.out.println();
+                    // todo 1 - по хорошему тоже надо break для одинаковости веток
             }
         }
     }
