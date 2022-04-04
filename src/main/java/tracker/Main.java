cf2dc151704653b14c128901e974061e6d8b62e0
lmotychenkov <motychenkovLA@gmail.com>
2022-04-04 11:23:29 +0300

    дз 3 замечания

--- src/main/java/tracker/Main.java
+++ src/main/java/tracker/Main.java
@@ -6,6 +6,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        // todo 1 - ни о чем не говорящее название
+        // todo 3 - размер почему-то 2, дефект это не строка, это две строки и число
         String[] array = new String[2];
         Scanner scanner = new Scanner(System.in);
         int i = 0;
@@ -18,14 +20,14 @@ public class Main {
                         System.out.println("Размер дефектов превышен");
                         break;
                     }
-                    final int WORK_WEEK = 5;
+                    final int WORK_WEEK = 5; // todo 0 - "больше недели" можно убрать
                     System.out.println("//Создание баг-репорта//");
                     System.out.println("Введите название дефекта");
                     String name = scanner.nextLine();
                     System.out.println("Введите критичность дефекта:\n" + "(trivial, minor, major, critical, blocker)");
                     String critical = scanner.nextLine();
                     System.out.println("Введите ожидаемое кол-во дней на исправление дефекта");
-                    int countDay = scanner.nextInt();
+                    int countDay = scanner.nextInt(); // todo 3 - перенос курсора
                     System.out.println("Исправление займет больше рабочей недели - " + (countDay > WORK_WEEK));
                     String defect = "Информация о заведенном дефекте:\nНомер дефекта - " + i +
                             "\nНазвание - " + name + "\nКритичность - " + critical +
@@ -35,6 +37,7 @@ public class Main {
                     i++;
                     break;
                 case "list":
+                    // todo 3 - дефекты наезжают друг на друга при выводе
                     System.out.println(Arrays.toString(array));
                     System.out.println();
                     break;
