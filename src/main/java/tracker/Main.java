5a58cb53d680ca1c607daa9f8f555b125976df32
eshnyagina <eshnyagina@t1-consulting.ru>
2022-04-05 17:24:18 +0300

    дз3

--- src/main/java/tracker/Main.java
+++ src/main/java/tracker/Main.java
@@ -1,20 +1,46 @@
 package tracker;
 
-import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите резюме дефекта:");
-        String summary = scanner.nextLine();
-        System.out.println("Введите критичность дефекта");
-        System.out.println("Minor\nMedium\nMajor\nCritical\nBlocker");
-        String severity = scanner.nextLine();
-        System.out.println("Введите ожидаемое количество дней на исправление дефекта");
-        int days = scanner.nextInt();
-        System.out.println("Итог:\n" + "Summary=" + summary +"\n" + "Severity=" + severity + "\n" + "Количество дней на исправление=" + days + "\n" + "Займет больше рабочей недели=" + (days>5));
+        List<String> bugs = new ArrayList<>();
+        int count = 0;
+        boolean isWork = true;
+        do {
+            System.out.println("Введите действие: добавить новый дефект (\"add\") или вывести список (\"list\") или выйти из программы (\"quit\") - главное меню ");
+            Scanner scanner = new Scanner(System.in);
+            switch (scanner.nextLine()){
+                case("add"):
+                    if(count <= 9){
+                        System.out.println("Введите резюме дефекта:");
+                        String summary = scanner.nextLine();
+                        System.out.println("Введите критичность дефекта");
+                        System.out.println("Minor\nMedium\nMajor\nCritical\nBlocker");
+                        String severity = scanner.nextLine();
+                        System.out.println("Введите ожидаемое количество дней на исправление дефекта");
+                        int days = scanner.nextInt();
+                        bugs.add(count + " | " + summary + " | " + severity + " | " + days);
+                        count++;
+                    }
+                    else {
+                        System.out.println("Превышено максимальное количество дефектов");
+                    }
+                    break;
+                case("list"):
+                    for (String bug : bugs) {
+                        System.out.println(bug);
+                    }
+                    break;
+                case("quit"):
+                    isWork = false;
+                    break;
+            }
+        }
+        while (isWork);
     }
 }
 
