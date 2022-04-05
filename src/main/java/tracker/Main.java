1e64b9b5ff60cc22b818e741d93ecbe9dc0f2941
ityupanov <igor.tyupanov@gmail.com>
2022-04-05 20:35:57 +0300

    Howe Work 2.Исправление ошибок

--- src/main/java/tracker/Main.java
+++ src/main/java/tracker/Main.java
@@ -3,31 +3,22 @@ package tracker;
 import java.util.Scanner;
 
 public class Main {
-    private static final int WEEK = 5;
+
     public static void main(String[] args) {
-        Scanner bugReport = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         String name;
         String priority;
-        int days = 0;
-        boolean daysOfWeek = false;
+        int days;
 
         System.out.println("Enter name:");
-        name = bugReport.nextLine();
+        name = scanner.nextLine();
         System.out.println("Enter priority: Trivial Minor Major Critical Blocker");
-        priority = bugReport.nextLine();
+        priority = scanner.nextLine();
+        System.out.println("Enter days: ");
+        days = scanner.nextInt();
 
-        if ((priority.equals("Trivial")) || (priority.equals("Minor"))
-                || (priority.equals("Major")) || (priority.equals("Critical")) || (priority.equals("Blocker"))) {
-            System.out.println("Enter days: ");
-            days = bugReport.nextInt();
-            if (days > WEEK) {
-                daysOfWeek = true;
-            }
-        } else {
-            System.err.println("Error type");
-        }
-
-        System.out.println("Name: "+name+" Priority: "+priority+" Days: "+days+" More work week?: "+daysOfWeek);
-        bugReport.close();
+        System.out.println("Info about bug:\n" + "Name: " + name + "\nPriority: "
+                + priority + "\nDays: " + days + "\nMore work week?: " + (days > 5));
+        scanner.close();
     }
 }
