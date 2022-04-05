129d8282f225e28edfc6680679ce763866a9b584
akapishena <akapishena@t1-consulting.ru>
2022-04-05 16:19:59 +0400

    ДЗ 3 (с исправлением)

--- src/main/java/tracker/Main.java
+++ src/main/java/tracker/Main.java
@@ -1,16 +1,17 @@
 package tracker;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        final int arrayMax = 10;
+        // todo 3 - 10 это константа - исправлено
+        int[] amountOfDays = new int[arrayMax]; // todo 1 - не говорящее название - исправлено
+        String[] description = new String[arrayMax];
+        String[] severity = new String[arrayMax];
 
-        // todo 3 - 10 это константа
-        int[] number = new int[10]; // todo 1 - не говорящее название
-        String[] description = new String[10];
-        String[] severity = new String[10];
-
-        int i = 0; // todo 1 - не говорящее название
+        int initialValue = 0; // todo 1 - не говорящее название - исправлено
 
         Scanner scanner = new Scanner(System.in);
         /* Главное меню */
@@ -21,13 +22,13 @@ public class Main {
 
             switch (operation) {
                 case "add":
-                    if (i > description.length - 1) { // todo 0 - "a > b - 1" и "a >= b" это то же самое но второе попроще читать
+                    if (initialValue >= arrayMax) { // todo 0 - "a > b - 1" и "a >= b" -исправлено
                         System.out.println("Не возможно добавить дефект");
                         break;
                     }
 
                     System.out.println("Введите описание дефекта");
-                    description[i] = scanner.nextLine();
+                    description[initialValue] = scanner.nextLine();
 
                     System.out.println("Выберите кричтичность дефектов из:\n" +
                             "-Blocker\n" +
@@ -35,21 +36,24 @@ public class Main {
                             "-Major\n" +
                             "-Minor\n" +
                             "-Trivial\n");
-                    severity[i] = scanner.nextLine();
+                    severity[initialValue] = scanner.nextLine();
 
                     System.out.println("Дни на исправление дефекта:");
-                    number[i] = scanner.nextInt();
-                    i++;
+                    amountOfDays[initialValue] = scanner.nextInt();
                     break;
 
                 case "list":
-                    // todo 5 - выводятся незаполненные дефекты
+                    // todo 5 - выводятся незаполненные дефекты - выводится одна строка
                     for (int j = 0; j < description.length; j++) {
-                        System.out.println("\tНомер дефекта: " + j + "\tОписание: " + description[i] +
-                                "\tКритичность: " + severity[i] + "\tКол-во дней на исправление: " + number[i]);
+                        if (description[j]==null) {
+                            break;
+                        }
+                        System.out.println("\tНомер дефекта: " + j + "\tОписание: " + description[j] +
+                                "\tКритичность: " + severity[j] + "\tКол-во дней на исправление: " + amountOfDays[j]);
                     }
                     System.out.println();
                     break;
+
 
                 case "quit":
                     System.out.println("Выход из системы");
@@ -57,7 +61,8 @@ public class Main {
 
                 default:
                     System.out.println("Такой операции не существует\n");
-                    // todo 1 - по хорошему надо тоже break для одинаковости веток
+                    break;
+                    // todo 1 - по хорошему надо тоже break для одинаковости веток - исправлено
             }
         }
     }
