c1a760bd387ee781028a31353e5dcdd4b4fa9e47
dvtarasov <dvtarasov@t1-consulting.ru>
2022-04-05 11:01:11 +0300

    ДЗ 2

---
+++ src/main/java/tracker/Main.java
@@ -0,0 +1,20 @@
+package tracker;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите резюме дефекта");
+        String name = scanner.nextLine();
+        System.out.println("Введите критичность дефекта");
+        System.out.println("Высокая, Средняя, Низкая");
+        String name1 = scanner.nextLine();
+        System.out.println("Введите срок исправления дефекта, в днях");
+        int number = scanner.nextInt();
+        scanner.nextLine();
+       boolean result = number >7;
+        System.out.println("Резюме дефекта: " + name + ", Критичность: " + name1 + ", Срок исправления: " +  number + " дней" + ", Займет больше рабочей недели: " + result);
+    }
+
+}
