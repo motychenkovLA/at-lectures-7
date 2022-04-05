fcceb470222d013b304e07826ff1116f1a7b0229
akapishena <akapishena@t1-consulting.ru>
2022-04-05 09:27:05 +0400

    ДЗ 3

--- src/main/java/tracker/Main.java
+++ src/main/java/tracker/Main.java
@@ -5,28 +5,58 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        int[] number = new int[2];
+        String[] description = new String[2];
+        String[] severity = new String[2];
+
+        int i = 0;
+
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите описание дефекта");
-        String description = scanner.nextLine();
+        /* Главное меню */
+        while (true) {
+            System.out.println("Введите операцию из списка:\nadd - добавить новый дефект; " +
+                    "\nlist - вывести список дефектов; \nquit - выход");
+            String operation = scanner.nextLine();
 
-        System.out.println("Выберите кричтичность дефектов из:\n" +
-                "-Blocker\n" +
-                "-Critical\n" +
-                "-Major\n" +
-                "-Minor\n" +
-                "-Trivial\n");
-        String severity = scanner.nextLine();
+            switch (operation) {
+                case "add":
+                    if (i > description.length - 1) {
+                        System.out.println("Не возможно добавить дефект");
+                        break;
+                    }
 
-        System.out.println("Дни на исправление дефекта:");
-        int number = scanner.nextInt();
-        scanner.nextLine();
+                    System.out.println("Введите описание дефекта");
+                    description[i] = scanner.nextLine();
 
-        System.out.println("На исправление дефекта " + description + " с критичностью " + severity + " необходимо " + number + " дней ");
+                    System.out.println("Выберите кричтичность дефектов из:\n" +
+                            "-Blocker\n" +
+                            "-Critical\n" +
+                            "-Major\n" +
+                            "-Minor\n" +
+                            "-Trivial\n");
+                    severity[i] = scanner.nextLine();
 
-        final int WEEK = 5;
-        boolean comparisonDays = number > WEEK;
+                    System.out.println("Дни на исправление дефекта:");
+                    number[i] = scanner.nextInt();
+                    i++;
+                    break;
 
-        System.out.println("Это займет больше недели?:" + comparisonDays);
+                case "list":
+                    for (int j = 0; j < description.length; j++) {
+                        System.out.println("\tНомер дефекта: " + j + "\tОписание: " + description[i] +
+                                "\tКритичность: " + severity[i] + "\tКол-во дней на исправление: " + number[i]);
+                    }
+                    System.out.println();
+                    break;
+
+                case "quit":
+                    System.out.println("Выход из системы");
+                    return;
+
+                default:
+                    System.out.println("Такой операции не существует\n");
+            }
+        }
     }
 }
 
