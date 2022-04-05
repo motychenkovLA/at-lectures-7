95fb6dced50524831d2c49d50e950d7129f57513
lmotychenkov <motychenkovLA@gmail.com>
2022-04-05 15:53:19 +0300

    дз 3 замечания

--- src/main/java/tracker/Main.java
+++ src/main/java/tracker/Main.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // todo 1 - форматирование не как у константы
         final int arrayMax = 10;
-        // todo 3 - 10 это константа - исправлено
-        int[] amountOfDays = new int[arrayMax]; // todo 1 - не говорящее название - исправлено
+        int[] amountOfDays = new int[arrayMax];
         String[] description = new String[arrayMax];
         String[] severity = new String[arrayMax];
 
-        int initialValue = 0; // todo 1 - не говорящее название - исправлено
+        int initialValue = 0; // todo 1 - все еще не говорящее название. что такое "начальное значение"? значение чего?
 
         Scanner scanner = new Scanner(System.in);
         /* Главное меню */
@@ -22,7 +22,7 @@ public class Main {
 
             switch (operation) {
                 case "add":
-                    if (initialValue >= arrayMax) { // todo 0 - "a > b - 1" и "a >= b" -исправлено
+                    if (initialValue >= arrayMax) {
                         System.out.println("Не возможно добавить дефект");
                         break;
                     }
@@ -39,18 +39,17 @@ public class Main {
                     severity[initialValue] = scanner.nextLine();
 
                     System.out.println("Дни на исправление дефекта:");
-                    amountOfDays[initialValue] = scanner.nextInt();
+                    amountOfDays[initialValue] = scanner.nextInt(); // todo 3 - нет переноса курсора на чтение
                     initialValue++;
                     break;
 
                 case "list":
-                    // todo 5 - выводятся незаполненные дефекты - выводится одна строка
-
                     for (int j = 0; j < initialValue; j++) {
                         System.out.println("\tНомер дефекта: " + j + "\tОписание: " + description[j] +
                                 "\tКритичность: " + severity[j] + "\tКол-во дней на исправление: " + amountOfDays[j]);
                     }
 
+                    // todo 0 - закомменченные куски кода лучше не оставлять в коммитах
                     /*Второй вариант*/
 //                    for (int j = 0; j < description.length; j++) {
 //                        if (description[j]==null) {
@@ -70,7 +69,6 @@ public class Main {
                 default:
                     System.out.println("Такой операции не существует\n");
                     break;
-                    // todo 1 - по хорошему надо тоже break для одинаковости веток - исправлено
             }
         }
     }
