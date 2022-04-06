97f039e9f4c3bc2a12a17c9babc5e4985b6af6a1
Julia Maple <klyonova1997@gmail.com>
2022-04-06 09:36:39 +0300

    homework3

--- src/main/java/tracker/Main.java
+++ src/main/java/tracker/Main.java
@@ -1,24 +1,64 @@
 package tracker;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Please, enter a description of the problem");
-        Scanner scanner = new Scanner(System.in);
-        String description = scanner.nextLine();
-        System.out.println("Please, enter a severity of the problem:\nS1 - Blocker;\nS2 - Critical;" +
-                "\nS3 - Major;\nS4 - Minor;\nS5 - Trivial");
-        String severity = scanner.nextLine();
-        System.out.println("How many days do you need to fix the problem?");
-        int numberOfDays =scanner.nextInt();
 
-        //check: 1 week include number of days for fix or not
-        int week = 7;
-        boolean daysForFixMoreThenWeek = numberOfDays > week;
 
-        System.out.println("Description: " + description + "\nSeverity: " +
-                severity + "\nWorkload in days: " + numberOfDays +
-                "\nNumber of days for fix more then one week: " + daysForFixMoreThenWeek);
+        String choiceCommand;
+        final int MAX_CAPACITY = 10;
+        int currentDefectNumber = 0;
+
+        int [] id = new int[MAX_CAPACITY];
+        String[] descriptionArray = new String[MAX_CAPACITY];
+        String[] severityArray = new String[MAX_CAPACITY];
+        String [] summaryArray = new String[MAX_CAPACITY];
+
+        while (true) {
+            System.out.println("Select a command: \"add\", \"list\", \"quit\"");
+            Scanner scanner = new Scanner(System.in);
+            choiceCommand = scanner.nextLine();
+
+            if (choiceCommand.equals("add")) {
+                if (currentDefectNumber != 10 ) {
+                    id[currentDefectNumber] = currentDefectNumber;
+
+                    System.out.println("Enter a resume of the problem");
+                    String description = scanner.nextLine();
+                    descriptionArray[currentDefectNumber] = "Resume: " + description;
+
+                    System.out.println("Please, enter a severity of the problem:\nS1 - Blocker;\nS2 - Critical;" +
+                            "\nS3 - Major;\nS4 - Minor;\nS5 - Trivial");
+                    String severity = scanner.nextLine();
+                    severityArray[currentDefectNumber] = "Severity: " + severity;
+
+                    System.out.println("How many days do you need to fix the problem?");
+                    int numberOfDays = scanner.nextInt();
+                    summaryArray[currentDefectNumber] = "Days for fix: " + numberOfDays;
+
+                } else {
+                    System.out.println("There is no place in array!");
+                    continue;
+                }
+
+                currentDefectNumber++;
+
+            } if (choiceCommand.equals("list")) {
+
+               for (int i = 0; i < currentDefectNumber; i++) {
+                   System.out.println(id[i] + " | " + descriptionArray[i] + " | " + severityArray[i] +
+                          " | " + summaryArray[i] );
+               }
+
+            } if (choiceCommand.equals("quit")) {
+
+                break;
+
+            }
+        }
     }
 }
+
