881433b36a91afe49f090794ae3382da705683b9
dvtarasov <dvtarasov@t1-consulting.ru>
2022-04-08 17:45:22 +0300

    ДЗ 3

--- src/main/java/helloWorld/Main.java
+++ src/main/java/helloWorld/Main.java
@@ -4,15 +4,67 @@ import java.util.Scanner;
 
 public class Main {
 
-    //предложить пользователю выбрать: добавить новый дефект ("add"), вывести список ("list") или выйти из программы ("quit") - главное меню
-    //при добавлении ввод параметров происходит как раньше
-    //после добавления, вернуться в главное меню
-    //программа максимально может хранить 10 дефектов, при попытке добавить больше, сообщает о невозможности и автоматически возвращает в главное меню
-    //при выводе списка дефекты отображаются в виде "номер (начиная с 0) | резюме | серьезность | количество дней на исправление"
-
     public static void main(String[] args) {
-        System.out.println("hello");
-    }
 
+        final int WORKING_WEEK = 5;
+
+        String[] bugTable = new String[10];
+
+
+        int i = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Выберите действие:" +
+                    "\n add - Завести дефект" +
+                    "\n list - Показать список дефектов" +
+                    "\n quit - Выход из программы");
+
+            switch (scanner.nextLine()) {
+
+                case "add":
+                    if (i >= bugTable.length) {
+                        System.out.println("Уже заведено 10 дефектов");
+                        break;
+                    }
+
+                    System.out.println("Введите резюме дефекта:");
+                    String bugReport = scanner.nextLine();
+
+                    System.out.println("Введите критичность дефекта (Высокая, Средняя, Низкая):");
+                    String critical = scanner.nextLine();
+
+                    System.out.println("Введите срок исправления дефекта, в днях:");
+                    int daysToRepair = scanner.nextInt();
+                    scanner.nextLine();
+
+                    bugTable[i] = "Номер дефекта " + i + ":" +
+                            "\n Описание дефекта: " + bugReport +
+                            "\n Критичность: " + critical +
+                            "\n Количество дней на исправление: " + daysToRepair +
+                            "\n Займет больше рабочей недели: " + (daysToRepair <= WORKING_WEEK) +
+                            "\n________________________________________________________";
+
+                    i++;
+                    break;
+
+                case "list":
+                    for (String q : bugTable) {
+                        if (q != null) System.out.println(q);
+                    }
+                    break;
+
+                case "quit":
+                    System.out.println("Quit");
+                    return;
+
+                default:
+                    System.out.println("Возврат в главное меню");
+                    break;
+            }
+        }
+
+    }
 
 }
