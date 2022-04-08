11924c0623df59c5127890d61ab9899436dddebb
tkoroleva <tkoroleva@t1-consulting.ru>
2022-04-08 16:07:30 +0300

    Дз 2 правка по замечаниям

--- src/main/java/tracker/Main.java
+++ src/main/java/tracker/Main.java
@@ -6,55 +6,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        final int NUM_BUG = 10;
-        String[] bugResume = new String[NUM_BUG];
-        String[] bugCrit = new String[NUM_BUG];
-
-        int[] bugDays = new int[NUM_BUG];
-        int countReq = 0; // переменная для количества дефектов
-        boolean isRun = true; // переменная для бесконечного цикла
-        while (isRun) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Выберите действие: \nAdd (Новый дефект)" +
-                    " List (Вывести список дефектов)" +
-                    " Quit (Вернуться в главное меню)");
-            String action = scanner.nextLine();
-            switch (action) {
-                case ("Add"): {
-                    if (countReq > NUM_BUG - 1) {
-                        System.out.println("Невозможно добавить больше 10 дефектов");
-
-                        break;
-                    }
-                    System.out.println("Введите резюме дефекта:");
-                    String resume = scanner.nextLine();
-
-                    System.out.println("Укажите критичность, \nHigh   Medium    Low:");
-                    String crit = scanner.nextLine();
-
-                    System.out.println("Введите, сколько дней потребуется для устранения:");
-                    int days = scanner.nextInt();
-                    scanner.nextLine();
-                    bugResume[countReq] = resume;
-                    bugCrit[countReq] = crit;
-                    bugDays[countReq] = days;
-                    countReq = countReq + 1;
-                    break;
-                }
-                case "List":
-                    // System.out.println(Arrays.toString(listbug));
-                    for (int i = 0; i < countReq; i++) {
-                        System.out.println("|" + i + "|" + bugResume[i] + "|" + bugCrit[i] + "|" + bugDays[i] + "|");
-                    }
-                    break;
-                case "Quit":
-                    isRun = false;
-                    break;
-                default:
-                    System.out.println("Операции не существует");
-                    break;
-            }
-        }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите резюме дефекта:");
+        String resume = scanner.nextLine();
+        System.out.println("Укажите критичность, \nHigh   Medium    Low:");
+        String crit = scanner.nextLine();
+        System.out.println("Введите, сколько дней потребуется для устранения:");
+        int days = scanner.nextInt();
+        scanner.nextLine();
+        // todo 1 - week это константа --- УЧТЕНО
+        final int week = 5;
+        // todo 1 - желательно это все в один sout собрать --- УЧТЕНО
+        // todo 1 - нужно чтоб оно не вылезало за белую линию и экран --- УЧТЕНО
+        System.out.println("Резюме: " + resume + "\nКритичность: " + crit + "\nСрок устранения: " + days + "\nУстранение займет больше рабочей недели?\n" +  (days > week));
     }
 }
 
