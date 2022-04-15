76865068b5822dd448f691e07463be2dd486168b
Albert G. Khusnetdinov <akhusnetdinov@t1-consulting.ru>
2022-04-15 16:24:46 +0300

    ДЗ_4.1

--- src/main/java/tracker/Tracker.java
+++ src/main/java/tracker/Tracker.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Tracker {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         while (true) {
             System.out.println("Введите операцию из списка:\nadd - добавить новый дефект\nlist - вывести список дефектов\nquit - выход");
             System.out.println();
