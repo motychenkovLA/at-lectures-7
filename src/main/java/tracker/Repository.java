a93c0f4d92f4214c3f1484ad5e14abb20d873e76
lmotychenkov <motychenkovLA@gmail.com>
2022-04-22 11:21:27 +0300

    дз 5 замечания

--- src/main/java/tracker/Repository.java
+++ src/main/java/tracker/Repository.java
@@ -3,8 +3,8 @@ package tracker;
 import java.util.Arrays;
 
 public class Repository {
-    private int size;
-    private static Defect[] listDefect;
+    private int size; // todo 1 - можно зафиналить
+    private static Defect[] listDefect; // todo 5 - общий массив на все репо
     private static int counterDefectSize = 0;
 
     public Repository(int size) {
@@ -12,10 +12,14 @@ public class Repository {
         listDefect = new Defect[size];
     }
 
+    // todo 1 - не говорящее название, существительное но почему-то метод. isFull как вариант
     public static boolean maxSize() {
         return getCounterDefectSize() >= listDefect.length;
     }
 
+    // todo 1 - какое-то огромное название,
+    //  вообще по канону максимальный размер - это capacity, а текущий - size,
+    //  но тут можно хотя бы просто count оставить
     public static int getCounterDefectSize() {
         return counterDefectSize;
     }
