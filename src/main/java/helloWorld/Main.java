b6dee193638ac96c7b08d52e01f5aac5b8d6d757
eshnyagina <eshnyagina@t1-consulting.ru>
2022-05-11 16:38:41 +0300

    дз7

--- src/main/java/helloWorld/Main.java
+++ src/main/java/helloWorld/Main.java
@@ -3,6 +3,11 @@ package helloWorld;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello");
+        int []array={4,5,6,7,9,12,17};
+        int a=0;
+        while(a< array.length){
+            System.out.println(array[a]);
+            a++;
+        }
     }
 }
