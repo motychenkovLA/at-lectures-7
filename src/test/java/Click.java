fbc03a831e817470e2219be5e4bd2c38af06cf5d
eshnyagina <eshnyagina@t1-consulting.ru>
2022-07-06 15:17:49 +0300

    дз15

---
+++ src/test/java/Click.java
@@ -0,0 +1,54 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+public class Click {
+    private static final By doubleClick = By.id("doubleClickBtn");
+    private static final By rightClick = By.id("rightClickBtn");
+    private static final By clickMe = By.xpath("//button[text()= 'Click Me']");
+    private static final By doubleClickTextXpath = By.xpath("//p[text()='You have done a double click']");
+    private static final By rightClickTextXpath = By.xpath("//p[text()='You have done a right click']");
+    private static final By clickTextXpath = By.xpath("//p[text()='You have done a dynamic click']");
+    private final WebDriver driver;
+
+    public Click(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void clickDoubleButton() {
+        WebElement doubleClickReal = driver.findElement(doubleClick);
+        new Actions(driver)
+                .doubleClick(doubleClickReal)
+                .build()
+                .perform();
+    }
+
+    public void clickRightButton() {
+        WebElement rightClickReal = driver.findElement(rightClick);
+        new Actions(driver)
+                .contextClick(rightClickReal)
+                .build()
+                .perform();
+    }
+
+    public void clickMe() {
+        WebElement clickMeReal = driver.findElement(clickMe);
+        new Actions(driver)
+                .click(clickMeReal)
+                .build()
+                .perform();
+    }
+
+    public boolean isHaveDoubleClickText() {
+        return !driver.findElements(doubleClickTextXpath).isEmpty();
+    }
+
+    public boolean isHaveRightClickText() {
+        return !driver.findElements(rightClickTextXpath).isEmpty();
+    }
+
+    public boolean isHaveClickText() {
+        return !driver.findElements(clickTextXpath).isEmpty();
+    }
+}
--- src/test/java/HomeWork.java
+++ src/test/java/HomeWork.java
@@ -1,85 +1,50 @@
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 public class HomeWork {
+    public static WebDriver driver;
+
     public static void main(String[] args) {
-        exerciseSecond();
+        click();
+        clickAlert();
     }
 
-    public static void exerciseFirst() {
+    public static void driverCall() {
         System.setProperty("webdriver.chrome.driver", "src/test/chromedriver/chromedriver.exe");
-        WebDriver webDriver= new ChromeDriver();
-        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        webDriver.get("https://demoqa.com/buttons");
+        driver = new ChromeDriver();
+    }
 
-        WebElement doubleClick=webDriver.findElement(By.id("doubleClickBtn"));
-        WebElement rightClick=webDriver.findElement(By.id("rightClickBtn"));
-        WebElement click= webDriver.findElement(By.xpath("//button[text()= 'Click Me']"));
+    public static void click() {
+        driverCall();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(7));
+        driver.get("https://demoqa.com/buttons");
+        Click click = new Click(driver);
+        click.clickDoubleButton();
+        click.clickRightButton();
+        click.clickMe();
 
-        new Actions(webDriver)
-                .doubleClick(doubleClick)
-                .contextClick(rightClick)
-                .click(click)
-                .build()
-                .perform();
-
-        boolean isHaveDoubleClickText = !webDriver.findElements(By.xpath(
-                "//p[text()='You have done a double click']")).isEmpty();
-        boolean isHaveRightClickText = !webDriver.findElements(By.xpath(
-                "//p[text()='You have done a right click']")).isEmpty();
-        boolean isHaveClickMeText = !webDriver.findElements(By.xpath(
-                "//p[text()='You have done a dynamic click']")).isEmpty();
-        webDriver.quit();
-
-        if (isHaveClickMeText&&isHaveDoubleClickText&&isHaveRightClickText){
+        if (click.isHaveDoubleClickText() && click.isHaveRightClickText()
+                && click.isHaveClickText()) {
             System.out.println("Тест пройден");
         } else {
             System.out.println("Тест не пройден");
         }
+        driver.quit();
     }
 
-    public static void exerciseSecond(){
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\eshnyagina\\IdeaProjects\\at-lectures-7\\src\\test\\chromedriver\\chromedriver.exe");
-        WebDriver webDriver= new ChromeDriver();
-        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-
-        WebDriverWait webDriverWait= new WebDriverWait(webDriver, Duration.ofSeconds(7));
-        webDriver.get("https://demoqa.com/alerts");
-
-        WebElement alertButton=webDriver.findElement(By.id("alertButton"));
-        WebElement alertTimerButton = webDriver.findElement(By.id("timerAlertButton"));
-        WebElement confirmButton = webDriver.findElement(By.id("confirmButton"));
-
-        alertButton.click();
-        webDriver.switchTo()
-                .alert().accept();
-
-        alertTimerButton.click();
-        webDriverWait.until(ExpectedConditions.alertIsPresent())
-                .accept();
-
-
-        confirmButton.click();
-        webDriver.switchTo()
-                .alert().dismiss();
-
-        boolean isHaveText = !webDriver.findElements(By.xpath(
-                "//span[contains(., 'Cancel')]")).isEmpty();
-        webDriver.quit();
-
-        if (isHaveText){
-            System.out.println("Тест пройден");
-        } else {
-            System.out.println("Тест не пройден");
-        }
-
+    public static void clickAlert() {
+        driverCall();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(7));
+        driver.get("https://demoqa.com/alerts");
+        ClickAlert clickAlert = new ClickAlert(driver);
+        clickAlert.clickAlertButton();
+        clickAlert.clickTimerAlertButton();
+        clickAlert.clickConfirmButton();
+        clickAlert.isHaveText();
+        driver.quit();
     }
+
 }
 
