be9b01427df10beb50be5a483d8bff1143f28741
easadulin <easadulin@t1-consulting.ru>
2022-08-01 16:31:32 +0300

    Дз № 16

---
+++ src/test/java/Page/NewPage.java
@@ -0,0 +1,33 @@
+package Page;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.Iterator;
+import java.util.Set;
+
+public class NewPage {
+    private final WebDriver driver;
+    private final By newTab = By.id("tabButton");
+
+    public NewPage(WebDriver driver) {
+        this.driver = driver;
+}
+    public void clickNewTab() {
+        driver.get("https://demoqa.com/browser-windows");
+        String demoqaDescriptor = driver.getWindowHandle();
+        WebElement newTabReal = driver.findElement(newTab);
+        newTabReal.click();
+
+        Set<String> handles = driver.getWindowHandles();
+        handles.remove(demoqaDescriptor);
+
+        Iterator<String> iterator = handles.iterator();
+        String newTabWindowDescriptor = iterator.next();
+
+        driver.switchTo().window(newTabWindowDescriptor);
+        driver.get("https://google.com");
+        driver.switchTo().window(demoqaDescriptor);
+    }
+}
