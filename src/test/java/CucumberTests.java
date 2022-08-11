344d74684db96caf3aab34d5d20bd68df25e4d3e
Julia Maple <klyonova1997@gmail.com>
2022-08-11 17:10:09 +0300

    homework Cucumber fux 1

--- src/test/java/CucumberTests.java
+++ src/test/java/CucumberTests.java
@@ -1,13 +1,13 @@
-//import io.cucumber.junit.Cucumber;
-//import io.cucumber.junit.CucumberOptions;
-//import org.junit.runner.RunWith;
-//
-//@RunWith(Cucumber.class)
-//@CucumberOptions(
-//        monochrome = true,
-//        features = {"src/test/resources/features"},
-//        glue = {"page"}
-//)
-//
-//public class CucumberTests {
-//}
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        monochrome = true,
+        features = {"src/test/resources/features"},
+        glue = {"page"}
+)
+
+public class CucumberTests {
+}
